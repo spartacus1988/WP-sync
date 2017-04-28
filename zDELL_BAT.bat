afb5dd6b8a4a8424af57d63da3c6c245b4aa1c81
mixxxxx <spartacusxxxxx@gmail.com>
2017-04-28 19:30:26 +0300

    first commit

---
+++ zDELL_BAT.bat
@@ -0,0 +1,13 @@
+﻿echo off  
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\offers_work.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\prototypes_work.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\prototypes_work_temp.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\offers_work_temp.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\offers_0.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\prototypes_0.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\instock_work.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\prices_work.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\instock_work_temp.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\prices_work_temp.xml
+del /Q Z:\home\wptest\www\wp-content\plugins\craftsync-craftmann\CraftSyncLog.txt
+end 
